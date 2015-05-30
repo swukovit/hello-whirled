76310063979611b05aa99b7c518cb98ecc84722b
John Williams <jodawi@gmail.com>
2015-05-30 01:14:53 -0700

    Added array version of SelectionSort

--- examples/src/sorting/SelectionSorter.java
+++ examples/src/sorting/SelectionSorter.java
@@ -3,7 +3,8 @@ package sorting;
 import java.util.ArrayList;
 
 public class SelectionSorter {
-    public void SelectionSort(ArrayList<Integer> a) {
+
+    public void SelectionSortArrayList(ArrayList<Integer> a) {
         if (a == null || a.size() <= 1)
             return;
 
@@ -13,6 +14,21 @@ public class SelectionSorter {
                     Integer temp = a.get(i);
                     a.set(i, a.get(j));
                     a.set(j, temp);
+                }
+            }
+        }
+    }
+
+    public void SelectionSortArray(int[] a) {
+        if (a == null || a.length <= 1)
+            return;
+
+        for (int i = 0; i < a.length - 1; ++i) {
+            for (int j = i + 1; j < a.length; ++j) {
+                if (a[j] < a[i]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
                 }
             }
         }
--- examples/test/src/sorting/SelectionSorterTest.java
+++ examples/test/src/sorting/SelectionSorterTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class SelectionSorterTest {
 
     @org.junit.Test
-    public void testSelectionSort() throws Exception {
+    public void testSelectionSortArrayList() throws Exception {
         ArrayList<Integer> actual = new ArrayList<>();
         Collections.addAll(actual, 1, 2, 99, -3, 5, 88, 2, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
 
@@ -20,8 +20,20 @@ public class SelectionSorterTest {
 
         SelectionSorter sorter = new SelectionSorter();
 
-        sorter.SelectionSort(actual);
+        sorter.SelectionSortArrayList(actual);
 
         assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testSelectionSortArray() throws Exception {
+        int [] actual   = new int[] { 1, 2, 99, -3, 5, 88, 2, 0, Integer.MAX_VALUE, Integer.MIN_VALUE };
+        int [] expected = new int[] { Integer.MIN_VALUE, -3, 0, 1, 2, 2, 5, 88, 99, Integer.MAX_VALUE };
+
+        SelectionSorter sorter = new SelectionSorter();
+
+        sorter.SelectionSortArray(actual);
+
+        assertArrayEquals(expected, actual);
     }
 }
